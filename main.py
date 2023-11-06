6e324b374ad0810a917c5c43a50273d09797ee0b
Andrew Baruch <andrewbaruch4@gmail.com>
2023-11-06 09:08:21 +0800

    Add Inserting to mongo, add environment variable for mongo

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import functions_framework
 import requests
 import json
 import re
+import os
+from pymongo import MongoClient
 
 @functions_framework.http
 def run(request):
@@ -24,29 +26,36 @@ def run(request):
     
     result = fetch_leetcode_questions(number)
 
-    return result, 200, {'Content-Type': 'application/json'}
+    insertToMongo(result)
+
+    return "Ok", 200
 
 
+def insertToMongo(questions):
+    # Connect to your MongoDB database
+    client = MongoClient(os.environ.get("MONGO_LINK"))
 
+    db = client[os.environ.get("MONGO_DB")]  
+    collection = db[os.environ.get("MONGO_COLLECTION")]
+
+    for question in questions:
+        collection.insert_one(question)
+    
+    return
 
 def get_leetcode_question_description(title_slug):
     url = "https://leetcode.com/graphql/"
-    headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-    }
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
     body = {
-    "query": """
+        "query": """
       query QuestionDescription($titleSlug: String!) {
         question(titleSlug: $titleSlug) {
           content
         }
       }
     """,
-    "variables": {
-      "titleSlug": title_slug
-    }
+        "variables": {"titleSlug": title_slug},
     }
 
     response = requests.post(url, headers=headers, json=body)
@@ -55,13 +64,53 @@ def get_leetcode_question_description(title_slug):
     question_description = str(data["data"]["question"]["content"])
 
     # Remove all HTML tags.
-    description = re.sub(r'<[^>]*>', '', question_description)
+    description = re.sub(r"<[^>]*>", "", question_description)
 
     # Remove all HTML entities.
-    description = re.sub(r'&[^;]*;', '', description)
+    description = re.sub(r"&[^;]*;", "", description)
 
     return description
 
+
+def get_leetcode_question_related_topics(question, desc):
+    topics =  [
+        "Array",
+        "Sorting",
+        "Linked List",
+        "Two Pointers",
+        "Heap",
+        "Stack",
+        "Queue",
+        "Matrix",
+        "Back Tracking",
+        "Binary Search",
+        "Bit Operation",
+        "Intervals",
+        "Tree",
+        "String",
+        "Sliding Window",
+        "Hash Table",
+        "Math",
+        "Binary Tree",
+        "Greedy",
+        "Depth First Search",
+        "Breadth First Search",
+        "Dynamic Programming",
+        "Divide and Conquer",
+        "Backtracking",
+        "Topological Sort",
+        "Trie",
+        "Union Find",
+        "Quickselect",
+        "Monotonic Stack",
+        ]
+
+    currq_topics = []
+    for item in topics:
+        if item in question["stat"]["question__title"] or item.lower() in desc.lower():
+            currq_topics.append(item)
+    return currq_topics
+    
 
 def fetch_leetcode_questions(n=10):
     counter = 0
@@ -76,17 +125,19 @@ def fetch_leetcode_questions(n=10):
         if counter >= n:
             break
 
-        # print(question["stat"]["frontend_question_id"])
+        curr_q_desc = get_leetcode_question_description(question["stat"]["question__title_slug"])
+
+        if curr_q_desc == "None":
+            continue
+
         curr_q = {
             "id": question["stat"]["frontend_question_id"],
             "title": question["stat"]["question__title"],
-            "category": question["stat"]["question__title_slug"],
-            "complexity": question["difficulty"]["level"],
-            "description" : get_leetcode_question_description(question["stat"]["question__title_slug"]),
-            "link": "https://leetcode.com/problems/" + question["stat"]["question__title_slug"]
+            "difficulty": question["difficulty"]["level"],
+            "description": curr_q_desc + f"\n\n Taken from Leetcode! Link: https://leetcode.com/problems/{question['stat']['question__title_slug']}",
+            "topics": get_leetcode_question_related_topics(question, curr_q_desc),
         }
-        # print(curr_q["description"])
-        # print(counter)
+
         if curr_q["description"] != "None":
             questions.append(curr_q)
             counter += 1
